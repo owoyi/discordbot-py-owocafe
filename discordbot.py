9df41a15333b48017764cd25c3df87d12cc50ae9
오떱이 <109456755+owoyi@users.noreply.github.com>
2023-02-16 22:33:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
@@ -13,17 +12,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
-
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
 
 
 try:
